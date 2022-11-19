f3a525dff9323f287551b331b975e780cefef34b
DarkWool <cooper27@live.com.mx>
2022-11-19 12:44:56 -0600

    Add a subtract method with tests

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -7,3 +7,11 @@ test("Sums two numbers", () => {
     expect(calculator.add(8500, 2089)).toEqual(10589);
     expect(calculator.add(1251012, 779276)).toEqual(2030288);
 });
+
+test("Subtracts two numbers", () => {
+    expect(calculator.subtract(2, 1)).toEqual(1);
+    expect(calculator.subtract(36, 15)).toEqual(21);
+    expect(calculator.subtract(234, 112)).toEqual(122);
+    expect(calculator.subtract(6340, 2119)).toEqual(4221);
+    expect(calculator.subtract(2890910, 1034400)).toEqual(1856510);
+})
