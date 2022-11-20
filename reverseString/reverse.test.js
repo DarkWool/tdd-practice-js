4255c272aa1b9550d925410ff76030154c2eb790
DarkWool <cooper27@live.com.mx>
2022-11-20 02:08:54 -0600

    Add another test for reverseString function

--- reverseString/reverse.test.js
+++ reverseString/reverse.test.js
@@ -3,6 +3,7 @@ import { reverseString } from "./reverse.js";
 test("Reverses a string successfully", () => {
     expect(reverseString("Hello world!")).toMatch("!dlrow olleH");
     expect(reverseString("Heroku should remain free.")).toMatch(".eerf niamer dluohs ukoreH");
+    expect(reverseString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat, erat pulvinar finibus tempus, libero ex finibus dui, a ultrices felis quam ac enim.")).toMatch(".mine ca mauq silef secirtlu a ,iud subinif xe orebil ,supmet subinif ranivlup tare ,tauqesnoc sullesahP .tile gnicsipida rutetcesnoc ,tema tis rolod muspi meroL");
 });
 
 test("Returns undefined when the argument is not a string", () => {
